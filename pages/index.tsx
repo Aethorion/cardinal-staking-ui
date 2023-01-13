cb26a0bc51e5bca3377c37de4a010d3a5ccdf6a8
John Nagy <aethorion@gmail.com>
2023-01-13 13:02:03 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,3 @@
-import { useStakePoolMetadataCtx } from 'providers/StakePoolMetadataProvider'
-
-import Homepage from '../components/Homepage'
-import StakePoolHome from './[stakePoolId]'
-
-function Home() {
-  const { stakePoolMetadata } = useStakePoolMetadataCtx()
-  return <div>{stakePoolMetadata ? <StakePoolHome /> : <Homepage />}</div>
-}
 
 export default Home
 import { Footer } from 'common/Footer'
